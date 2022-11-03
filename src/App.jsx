1e091cf3e638d0b06428dc1a398ad0fadaebd762
RidoCoorporate <ridho@gudangkripto.id>
2022-11-03 17:28:21 +0700

    add switch theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import logo from './logo.svg';
+import ReactSwitch from 'react-switch';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+  const [theme, setTheme] = useState(null);
+
+  const themeSwitch = () => {
+    if (theme === 'dark') {
+      setTheme('light');
+      localStorage.setItem("theme", "light");
+    } else {
+      setTheme('dark');
+      localStorage.setItem("theme", "dark");
+    }
+
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem("theme") === 'dark') {
+      setTheme('dark');
+    }
+    else {
+      setTheme('light');
+    }
+  }, [])
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
   return (
-    <div className='container mx-auto bg-gray-200 rounded-xl shadow'>
-      <div className="grid grid-cols-3 gap-4">
-        <div>
-          <img src={logo} alt="" className='w-36' />
+    <div className="h-screen flex items-center justify-center px-8">
+      <div className='max-w-[600px] shadow-md p-6 rounded-md bg-white dark:bg-gray-600'>
+        <div className="flex items-start sm:items-center mb-4">
+          <h1 className="text-4xl flex-1 dark:text-white">Tailwind Dark/Light Mode</h1>
+          <ReactSwitch onChange={themeSwitch} checked={theme === 'dark'} />
         </div>
-        <div>
-          <p className='text-3xl text-gray-700 font-bold mb-5'>
-            welcome
-          </p>
-        </div>
-        <div>
-          <p className='text-gray-500 text-lg'>
-            master react with tailwind
-          </p>
-        </div>
-        <div className='col-span-2 text-center'>
-          <p className='text-3xl text-gray-700 font-bold mb-5'>
-            welcome
-          </p>
-        </div>
-        <div>
-          <p className='text-gray-500 text-lg'>
-            master react with tailwind
-          </p>
-        </div>
+        <p className="text-md mb-4 dark:text-white">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem totam ipsum id laudantium rerum voluptas et aspernatur impedit eaque fugit. Aperiam error officia libero, odit labore ex ipsa beatae! Enim.
+        </p>
+        <button className="py-2 px-4 bg-black dark:bg-white text-white rounded-md dark:text-black ">
+          lets go
+        </button>
       </div>
     </div>
+
   );
 }
 
